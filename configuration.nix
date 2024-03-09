80c7f4db5c948b19b65eadada9a0140ab7c85656
Marcin Mazurkiewicz <marcin@mazurkiewicz.io>
2024-03-09 06:13:46 +0100

    clean config

--- configuration.nix
+++ configuration.nix
@@ -1,32 +1,22 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, lib, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       /etc/nixos/hardware-configuration.nix
       /etc/nixos/machine-configuration.nix
       <home-manager/nixos>
     ];
 
-  
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  system.stateVersion = "23.11";  
+
+  time.timeZone = "Europe/Warsaw";
 
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Warsaw";
-
-  # Select internationalisation properties.
+  # Internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pl_PL.UTF-8";
     LC_IDENTIFICATION = "pl_PL.UTF-8";
@@ -45,9 +35,6 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  services.logind.lidSwitch = "hibernate";
-  services.logind.lidSwitchDocked = "ignore";
   
   # Configure keymap in X11
   services.xserver = {
@@ -57,6 +44,10 @@
 
   # Configure console keymap
   console.keyMap = "pl2";
+
+  # Hibernation
+  services.logind.lidSwitch = "hibernate";
+  services.logind.lidSwitchDocked = "ignore";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -70,14 +61,9 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
+  # Udev rules for ZSA keyboard
   services.udev.extraRules = ''
     # Rules for Oryx web flashing and live training
     KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
@@ -105,10 +91,7 @@
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
   '';
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # User account
   users.users.m2 = {
     isNormalUser = true;
     description = "m2";
@@ -118,9 +101,12 @@
     ];
   };
 
+  # Zsh with powerlevel10k
   programs.zsh.enable = true;
   programs.zsh.promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
 
+
+  # Home manager settings
   home-manager.useGlobalPkgs = true;
   
   home-manager.users.m2 = { pkgs, ... }: {
@@ -212,12 +198,12 @@
       "google-chrome"
     ];
 
+  # Custom fonts
   fonts.packages = with pkgs; [
     meslo-lgs-nf
   ];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # List packages installed in system profile.
   environment.systemPackages = with pkgs; [
     vim
     wget
@@ -257,33 +243,4 @@
       ];
     })
   ];
-
- 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
-
 }
